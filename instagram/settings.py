5dc5496261f689c79a3217bc0c388ca1f94416b3
Ajani Timothy <timothyinioluwa@gmail.com>
2022-01-14 00:31:11 +0100

    second commit

--- instagram/settings.py
+++ instagram/settings.py
@@ -118,7 +118,7 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
